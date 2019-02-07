a175643694dd73b68fd4cf99b451cf5b334bec5a
Srija S <srijakumar7@gmail.com>
2019-02-07 03:51:35 +0000

    User Sign Up form

--- addressbar_sinatraapp/app/controllers/users_controller.rb
+++ addressbar_sinatraapp/app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UserController < ApplicationController
     erb :'/users/signup'
   end
 
+  post '/signup' do
+
+    post '/signup'do
+        if params[:password] == params[:password_confirm]
+
+          @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+          session[:user_id] = @user.id
+          redirect to "/users/#{@user.id}"
+        else
+          redirect to "/signup"
+        end
+      end
+
+
 end
